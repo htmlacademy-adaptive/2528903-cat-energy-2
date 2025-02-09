25c57198f80758adf01b9315b654cdf9c8b1b02b
Avaqulon <feadosy24@gmail.com>
2025-02-09 17:57:35 +0300

    Module5 (#6)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__toogle');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('navigation--closed')) {
+    navMain.classList.remove('navigation--closed');
+    navMain.classList.add('navigation--opened');
+  } else {
+    navMain.classList.add('navigation--closed');
+    navMain.classList.remove('navigation--opened');
+  }
+});
